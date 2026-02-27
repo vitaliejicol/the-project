68f47846b085c0e6c2c1234f3f39825a11f9dc8d
vitalie.jicol <vitaliejicol89@gmail.com>
2026-02-27 17:13:35 -0600

    Added the Terraform cluster configuration

---
+++ infrastructure/cluster/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "vitaliejicol"
+
+    workspaces {
+      name = "test"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.38.0"
+    }
+
+    rhcs = {
+      source  = "terraform-redhat/rhcs"
+      version = ">= 1.7.2"
+
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "rhcs" {
+  token = var.rhcs_token
+}
---
+++ infrastructure/cluster/variables.tf
@@ -0,0 +1,5 @@
+variable "rhcs_token" {
+  description = "RHCS API token for authentication"
+  type        = string
+  
+}
