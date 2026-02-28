0f833df5c162efca36b673386c5c31d57b68da91
vitalie.jicol <vitaliejicol89@gmail.com>
2026-02-28 09:36:42 -0600

    Made various changes

---
+++ infrastructure/cluster/terraform/main.tf
@@ -0,0 +1,63 @@
+######### Create ECR ###########
+data "aws_caller_identity" "current" {}
+
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "private"
+
+  repository_read_write_access_arns = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/terraform_cloud"]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+  }
+}
+
+######## Create ROSA HCP Cluster ###########
+
+# module "vpc" {
+#   source = "terraform-redhat/rosa-hcp/rhcs//modules/vpc"
+
+#   name_prefix              = "paymentology-vpc"
+#   availability_zones_count = 3
+# }
+
+# module "hcp" {
+#   source = "terraform-redhat/rosa-hcp/rhcs"
+
+#   cluster_name           = "paymentology-cluster"
+#   openshift_version      = "4.20.8"
+#   machine_cidr           = "10.0.0.0/16"
+#   aws_subnet_ids         = ["${module.vpc.public_subnets[0]}", "${module.vpc.public_subnets[1]}", "${module.vpc.public_subnets[2]}", "${module.vpc.private_subnets[0]}", "${module.vpc.private_subnets[1]}", "${module.vpc.private_subnets[2]}"]
+#   aws_availability_zones = module.vpc.availability_zones
+#   replicas               = 3
+
+#   // STS configuration
+#   create_account_roles  = true
+#   account_role_prefix   = "paymentology-cluster-account"
+#   create_oidc           = true
+#   create_operator_roles = true
+#   operator_role_prefix  = "paymentology-cluster-operator"
+
+#   create_admin_user = true
+#   admin_credentials_username = var.admin_credentials_username
+#   admin_credentials_password = var.admin_credentials_password
+# }
